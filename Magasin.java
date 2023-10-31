9c4f0279f28209e6359966d9749de29892b86299
Cyrine Attia <cyrine.attia@esprit.tn>
2023-10-31 21:55:16 +0100

    classe Magasin crée

---
+++ Magasin.java
@@ -0,0 +1,47 @@
+public class Magasin {
+
+    int identifiant;
+    String adresse;
+
+    final int CAPACITE_PRD=50;
+
+    Produit [] tabprod=new Produit[CAPACITE_PRD];
+
+    int compprod;
+
+    public Magasin()
+    {}
+
+    public Magasin(int id,String ad)
+    {
+        identifiant=id;
+        adresse=ad;
+    }
+
+    public void ajouter(Produit p)
+    {
+       if(compprod<CAPACITE_PRD) {
+           tabprod[compprod] = p;
+           compprod++;
+           System.out.println("produit ajouté ");
+       }
+       else{
+
+           System.out.println("Magasin plein");
+       }
+    }
+    public void afficher()
+    {
+
+        System.out.println("identifiant :"+this.identifiant+"adresse :"+adresse);
+        for (int i=0;i<compprod;i++)
+        {
+
+            System.out.println("Marque :"+tabprod[i].marque);
+            System.out.println("libelle :"+tabprod[i].libelle);
+            System.out.println("date :"+tabprod[i].dateexp);
+            System.out.println("prix :"+tabprod[i].prix);
+            System.out.println("Identifiant :"+tabprod[i].identifiant);
+        }
+    }
+}
